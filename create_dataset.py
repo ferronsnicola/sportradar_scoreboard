19dbd0daab18834f880f61bd275c588ff345c185
ferronsnicola <ferronsnicola@gmail.com>
2021-05-11 00:01:40 +0200

    filter frames with same board
    i have a lot of too similar frames, with the same board, so I filter keeping only the distinct ones

--- create_dataset.py
+++ create_dataset.py
@@ -7,6 +7,10 @@ with open('data/top-100-shots-rallies-2018-atp-season-scoreboard-annotations.jso
 
 vc = cv.VideoCapture('data/top-100-shots-rallies-2018-atp-season.mp4')
 count = 0
+last_p1 = None
+last_p2 = None
+last_s1 = None
+last_s2 = None
 
 while True:
     ret, frame = vc.read()
@@ -17,10 +21,12 @@ while True:
             s1 = data[str(count)]['score_1']
             s2 = data[str(count)]['score_2']
             bbox = data[str(count)]['bbox']
+            if not (p1 == last_p1 and p2 == last_p2 and s1 == last_s1 and s2 == last_s2):
+                last_p1, last_p2, last_s1, last_s2 = p1, p2, s1, s2
 
-            cv.imshow('scoreboard', frame[int(bbox[1]):int(bbox[3]), int(bbox[0]):int(bbox[2])])
-            print(count)
-            cv.waitKey(0)
+                cv.imshow('scoreboard', frame[int(bbox[1]):int(bbox[3]), int(bbox[0]):int(bbox[2])])
+                print(count)
+                cv.waitKey(0)
         else:
             print('nothing at frame ' + str(count))
         count += 1
