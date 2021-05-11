5392e0060a93ca700d9bb5f5c0497362d83e5422
ferronsnicola <ferronsnicola@gmail.com>
2021-05-12 00:58:40 +0200

    Update main.py
    added a little heuristic to detect the serving player

--- yolov5/main.py
+++ yolov5/main.py
@@ -8,6 +8,7 @@ if __name__ == '__main__':
     pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
     cap = cv.VideoCapture('../data/top-100-shots-rallies-2018-atp-season.mp4')
     count = 0
+    serving_crop_witdh = 10
     model = detect.load_model()
 
     while cap.isOpened():
@@ -56,9 +57,30 @@ if __name__ == '__main__':
         text2 = pytesseract.image_to_string(second)
         print(text2)
 
+        # EXTRACT THE TWO PLAYER NAMES AND THEIR SCORES
+        player_1 = ''
+        player_2 = ''
+        score_1 = ''
+        score_2 = ''
+
+        # WHO IS SERVING?
+        serving = ''
+        if text1.startswith('>') or text1.startswith('»'):
+            serving = player_1
+        elif text2.startswith('>') or text2.startswith('»'):
+            serving = player_2
+        else:  # extract a little portion at the beginning of the lines, choose the brightest one
+            first_crop = scoreboard[0:scoreboard.shape[0]//2, 0:serving_crop_witdh]
+            second_crop = scoreboard[scoreboard.shape[0]//2:scoreboard.shape[0]-1, 0:serving_crop_witdh]
+            sum1 = first_crop.sum()
+            sum2 = second_crop.sum()
+            serving = player_1 if sum1 > sum2 else player_2
+
         cv.imshow('binary', binary)
         cv.imshow('first', first)
         cv.imshow('second', second)
+        cv.imshow('first_crop', first_crop)
+        cv.imshow('second_crop', second_crop)
         cv.imshow('detected_scoreboard', scoreboard)
         cv.waitKey(0)
 
