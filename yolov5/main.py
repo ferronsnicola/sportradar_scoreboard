8edaa8484e8594bc5208f18f9c5aaefb07603136
ferronsnicola <ferronsnicola@gmail.com>
2021-05-11 23:57:13 +0200

    add tesseract ocr
    it works, but not very well

--- yolov5/main.py
+++ yolov5/main.py
@@ -1,8 +1,10 @@
 import detect
 import cv2 as cv
+import pytesseract
 
 if __name__ == '__main__':
 
+    pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
     cap = cv.VideoCapture('../data/top-100-shots-rallies-2018-atp-season.mp4')
     count = 0
     model = detect.load_model()
@@ -12,6 +14,10 @@ if __name__ == '__main__':
         original_frame = frame.copy()
 
         scoreboard = detect.detect_score_board(model, original_frame)
+
+        # OCR
+        text = pytesseract.image_to_string(scoreboard)
+        print(text)
 
         cv.imshow('detected_scoreboard', scoreboard)
         cv.waitKey(0)
