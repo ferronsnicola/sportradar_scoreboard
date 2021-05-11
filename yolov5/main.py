7c77dfc24001b1738b1fc3786a49b6e9c09c794a
ferronsnicola <ferronsnicola@gmail.com>
2021-05-12 00:03:16 +0200

    Update main.py
    a bit of preprocessing, it does not do big differences

--- yolov5/main.py
+++ yolov5/main.py
@@ -1,6 +1,7 @@
 import detect
 import cv2 as cv
 import pytesseract
+import numpy as np
 
 if __name__ == '__main__':
 
@@ -16,6 +17,13 @@ if __name__ == '__main__':
         scoreboard = detect.detect_score_board(model, original_frame)
 
         # OCR
+        scoreboard = cv.cvtColor(scoreboard, cv.COLOR_BGR2GRAY)
+        # scoreboard = cv.Canny(scoreboard, 100, 200)
+        scoreboard = cv.threshold(scoreboard, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)[1]
+
+        kernel = np.ones((2, 2), np.uint8)
+        scoreboard = cv.erode(scoreboard, kernel, iterations=1)
+
         text = pytesseract.image_to_string(scoreboard)
         print(text)
 
